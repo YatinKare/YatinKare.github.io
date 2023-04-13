84c9680e54cde4d5cf46e6e48bbd9c0ad339d77f
YatinK <yatin.kare@gmail.com>
2023-04-13 11:50:32 -0400

    Initial setup

---
+++ yatin-portfolio/src/components/navigation.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import type { PageProps } from "gatsby"
+
+
+
+const Navigation: React.FC<PageProps> = () => {
+  return (
+   <div className="main">
+    
+   </div> 
+  )
+}
+
+export default Navigation;
--- yatin-portfolio/src/pages/index.tsx
+++ yatin-portfolio/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-
+import Navigation from "../components/navigation"
 
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <p>Hello world</p>
+   <div className="main">
+    <Navigation />
+    <div className="intro">
+      <h1>Welcome to my portfolio</h1>
+      <p>Im a 18 year old highschool senior who loves technology, music, and photography</p>
+       {/* <img src={profile}></img> */}
+       <section className="certs">
+        <h1>Here's what I'm certified for</h1>
+        <div className="imagerow">
+          
+        </div>
+       </section>
+    </div>
+   </div> 
   )
 }
 
