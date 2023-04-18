78ffc2b8e54d59751d1d2ecc50add88164de0efa
YatinK <yatin.kare@gmail.com>
2023-04-18 00:38:32 -0400

    Navigation header, intro and certifications sections

---
+++ yatin-portfolio/src/components/experience.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import type { PageProps } from "gatsby"
+
+
+
+const Experience: React.FC<PageProps> = () => {
+  return (
+   <div className="main">
+    
+   </div> 
+  )
+}
+
+export default Experience;
--- yatin-portfolio/src/components/navigation.tsx
+++ yatin-portfolio/src/components/navigation.tsx
@@ -1,12 +1,17 @@
 import * as React from "react"
-import type { PageProps } from "gatsby"
+import { Link, PageProps } from "gatsby"
+import profile from "../images/profile.jpeg";
 
-
+import * as styles from "./navigation.module.css";
 
 const Navigation: React.FC<PageProps> = () => {
   return (
-   <div className="main">
-    
+   <div className={styles.main}>
+    <img src={profile} className={styles.image} />
+    <div className={styles.links}>
+      <Link to='/posts' className={styles.link}>/posts</Link>
+       <Link to='/contact' className={styles.link}>/contact</Link>     
+    </div>
    </div> 
   )
 }
